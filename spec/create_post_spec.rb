d6694020c8cce3e6f90201e61acbbbc2bc85a7cd
vcavallo <vcavallo@gmail.com>
2014-02-01 12:56:45 -0500

    write initial post specs

---
+++ spec/create_post_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Post do
+
+  it "should have a field" do
+    pending "need to add field attribute"
+  end
+
+  it "should have months of experience" do
+    pending "need to add months experience attribute"
+  end
+
+  it "should have inability" do
+    pending "need to add inability attribute"
+  end
+
+end
