a4084c75ec3b2634930f0d68212fe16d57a24c20
vcavallo <vcavallo@gmail.com>
2014-02-01 19:31:34 -0500

    switch to postgres in all environments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ ImpostorRoster::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'posts#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
