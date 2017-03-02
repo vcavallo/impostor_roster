83176068344090e202e677e1e85724a1b7fa4c6d
vinney cavallo <vcavallo@gmail.com>
2017-03-02 12:45:55 -0500

    Add better placeholders and confirm to post submit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,42 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
+//= require jquery.turbolinks
+//= require jquery_ujs
+
+$(document).ready(function() {
+  var inabilityInput = $(".new-submission input[name='post[inability]']");
+  var inabilityHints = [
+    "Wake up on time",
+    "Right-click on a Mac",
+    "Pump gas",
+    "Swipe a MetroCard reliably",
+    "Remember which 'their' is correct",
+    "Google stuff effectively",
+  ]
+
+  inabilityInput.attr(
+    "placeholder",
+    inabilityHints[Math.floor(Math.random()*inabilityHints.length)]
+  );
+
+  $(".new-submission form input[type=submit]").click(function(event) {
+    event.preventDefault();
+    var field = $(".new-submission input[name='post[category]']").val();
+    var months = $(".new-submission input[name='post[months_experience]']").val();
+    var inability = $(".new-submission input[name='post[inability]']").val();
+    var post = "Someone in the " + field + " field for " + months + " months still can't " + inability;
+
+    if(confirm("Your post will appear like this:\n'" + post + "'.\nOk? Or do you need to fix it?")) {
+      $(".new-submission form").submit();
+    } else {
+      return false;
+    }
+  });
+
+});
+
 //= require_tree .
+//= require turbolinks
+
