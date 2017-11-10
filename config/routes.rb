d92d095ccdc7bc7fbb0957c33c22c4227eb94f72
vinney cavallo <vcavallo@gmail.com>
2017-11-10 11:02:54 -0500

    Add resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+    # Example of regular route:
+    #   get 'products/:id' => 'catalog#view'
+
+  resources :posts, only: [:index, :new, :create, :show]
+  resources :categories, only: [:index, :show]
+  get 'about' => 'pages#about', :as => :about
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
