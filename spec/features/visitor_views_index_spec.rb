eb46be7b14a37d5980972542e4395f3f557073c2
vinney cavallo <vcavallo@gmail.com>
2017-11-10 10:59:46 -0500

    Add basic feature specs for main paths

--- spec/features/visitor_views_index_spec.rb
+++ spec/features/visitor_views_index_spec.rb
@@ -5,5 +5,20 @@ feature "Viewing the listing of all posts" do
     visit root_path
     expect(page).to have_content("All Posts")
   end
+
+  scenario "Visitor sees individual posts on index" do
+    ruby_category = FactoryGirl.create(:ruby_category)
+    ruby_post = FactoryGirl.create(:ruby_post)
+    visit root_path
+    expect(page).to have_content(ruby_post.inability)
+  end
+
+  scenario "Visitor clicks on a post" do
+    ruby_category = FactoryGirl.create(:ruby_category)
+    ruby_post = FactoryGirl.create(:ruby_post)
+    visit root_path
+    click_on(ruby_post.inability)
+    expect(page).to have_content("I'm in the #{ ruby_category.name } field")
+  end
 end
 
